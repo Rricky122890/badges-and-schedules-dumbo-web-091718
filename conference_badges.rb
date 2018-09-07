1f279e5651d053485eb1b06044d33f348ab3035f
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-07 20:14:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -28,3 +28,28 @@ def printer(name)
  assign_rooms(name)
 
 end
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+ def batch_badge_creator (array)
+array.collect do |i|
+  badge_maker(i)
+  end
+end
+ def assign_rooms(array)
+  room=0
+  array.collect do |i|
+    room+=1
+    "Hello, #{i}! You'll be assigned to room #{room}!"
+  end
+end
+ def printer (array)
+  batch_badge_creator(array).each do |i|
+    puts i
+  end
+  assign_rooms(array).each do |i|
+    puts i
+  end
+end
