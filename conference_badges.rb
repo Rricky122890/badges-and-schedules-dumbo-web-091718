e8de619970b7db0558fef2d9223ba2e14205db4a
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-07 20:01:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+puts "Hello my name is #{name}"
+end 
+
+def batch_badge_maker (name)
+
+name.each do |person|  
+ puts "Hello, my name is #{person}"
+
+end
+
+end
+
+def assign_rooms (name)
+
+i = 0
+name.each do |person|
+i+=1
+puts "Hello #{person}, you are assigned to room #{i}"
+
+end
+end
+
+
+
+def printer(name)
+ batch_badge_maker(name)
+ assign_rooms(name)
+
+end
